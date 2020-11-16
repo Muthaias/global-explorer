8e6cfbd813f841d69917af98ddb5e65fecc84ca2
Mattias Nyberg <mattiasnyberg@localhost.localdomain>
2020-11-16 08:28:56 +0100

    wip: add log control

--- assets/utils.js
+++ assets/utils.js
@@ -1,12 +1,25 @@
 function createLog(elementId) {
     const element = document.getElementById(elementId)
     const log = []
+    let isOpen = false;
+    element.classList.add("closed")
 
     const combineEntries = (entries) => {
         return entries.map(entry => `<div>${entry}</div>`).join("")
     }
     return (...entries) => {
         log.push(entries)
-        element.innerHTML = log.map(entries => `<div class="log-entry">${combineEntries(entries)}</div>`).join("")
+        const controlId = `log-control-${elementId}`
+        const control = `<div class="log-control" id="${controlId}">console</div>`
+        element.innerHTML = control + log.map(entries => `<div class="log-entry">${combineEntries(entries)}</div>`).join("")
+        const controlElement = document.getElementById(controlId)
+        controlElement.addEventListener("click", () => {
+            isOpen = !isOpen
+            if (isOpen) {
+                element.classList.remove("closed")
+            } else {
+                element.classList.add("closed")
+            }
+        })
     }
 }
