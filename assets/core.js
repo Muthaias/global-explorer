316fb4c827a4a2845722f2c2bec0bc598c2ee250
Mattias Nyberg <mattiasnyberg@localhost.localdomain>
2020-11-22 14:10:58 +0100

    wip: move system to a websockets based api

--- assets/core.js
+++ assets/core.js
@@ -1,12 +1,11 @@
 class GlobalExplorer {
-    constructor(api, onUpdate = () => {}, log = () => {}) {
+    constructor(api, onUpdate = () => {}) {
         this._api = api
         this._player = null
         this._content = {}
         this._game = null
         this._version = "unknown"
         this._onUpdate = onUpdate
-        this._log = log
     }
 
     get player() {
@@ -47,5 +46,82 @@ class GlobalExplorer {
             this._api.version(),
         ])
         this._onUpdate()
+    }
+
+    static async from_websocket(
+        path,
+        onUpdate = () => {}
+    ) {
+        class _Api {
+            constructor(socket) {
+                this._socket = socket
+                console.log(socket)
+                this._messages = {}
+                this._counter = 0
+                this._socket.onmessage = (event) => {
+                    const rpc = JSON.parse(event.data)
+                    if (this._messages[rpc._id]) {
+                        this._messages[rpc._id](rpc.response)
+                    }
+                };
+            }
+
+            async init() {
+                return this._rpc({type: "init"})
+            }
+
+            async player() {
+                return this._rpc({type: "player"})
+            }
+
+            async content() {
+                return this._rpc({type: "content"})
+            }
+
+            async game() {
+                return this._rpc({type: "game"})
+            }
+
+            async version() {
+                return this._rpc({type: "version"})
+            }
+
+            async action(action) {
+                return this._rpc({
+                    type: "action",
+                    action: action
+                })
+            }
+
+            async _rpc(data) {
+                return await new Promise((resolve, reject) => {
+                    const id = this._counter++
+                    const stringData = JSON.stringify({
+                        ...data,
+                        _id: id,
+                    })
+                    this._socket.send(stringData)
+                    const timeout = setTimeout(() => {
+                        delete this._messages[id]
+                        console.warn("Timeout: ", id, data)
+                        reject("Timeout error")
+                    }, 1000)
+                    this._messages[id] = (data) => {
+                        clearTimeout(timeout)
+                        delete this._messages[id]
+                        resolve(data)
+                    }
+                    
+                })
+            }
+        }
+        const ws = new WebSocket(path)
+        return new Promise((resolve, reject) => {
+            ws.onopen = async () => {
+                const api = new _Api(ws)
+                await api.init()
+                resolve(new GlobalExplorer(api, onUpdate))
+            }
+        })
     }
 }
---
+++ web_service.py
@@ -0,0 +1,107 @@
+from game.node_manager import NodeManager
+from game.trotter import Player, Account, Transaction
+from game_runner import create_game_runner
+import asyncio
+import websockets
+import json
+
+
+class Server:
+    def __init__(self, handlers=None):
+        self.__handlers = {} if handlers is None else handlers
+        self.__socket_id = {}
+
+    def socket_id(self, websocket):
+        return websocket
+
+    def add_handler(self, id, func):
+        self.__handlers[id] = func
+
+    async def handle(self, websocket, path):
+        async for message in websocket:
+            data = json.loads(message)
+            try:
+                handler = self.__handlers[data["type"]]
+                await handler(data, websocket, path)
+            except Exception as e:
+                print(e)
+
+    def serve(self, host="localhost", port=8765):
+        return websockets.serve(self.handle, host, port)
+
+
+def create_server():
+    node_manager = NodeManager.from_paths([
+        "data/defaults.yaml",
+        "data/polacks.yaml",
+        "data/barkeep.yaml",
+        "data/uppsala.yaml",
+        "data/stockholm.yaml"
+    ])
+
+    runners = {}
+    server = Server()
+
+    @server_handler(server)
+    def init(data, id):
+        name = data.get("name", "Dirk Smallwood")
+        initial_funds = Transaction(1000)
+        player = Player(
+            name=name,
+            account=Account([initial_funds]),
+            skills=[]
+        )
+        runners[id] = create_game_runner(
+            node_manager=node_manager,
+            player=player,
+        )
+        return {
+            "name": player.name,
+            "account_balance": player.account.balance
+        }
+
+    @server_handler(server)
+    def version(data, id):
+        return runners[id].version()
+
+    @server_handler(server)
+    def content(data, id):
+        return runners[id].content()
+
+    @server_handler(server)
+    def player(data, id):
+        return runners[id].player()
+
+    @server_handler(server)
+    def game(data, id):
+        return runners[id].game()
+
+    @server_handler(server)
+    def action(data, id):
+        return runners[id].action(data.get("action"))
+
+    return server
+
+
+def server_handler(server):
+    def _server_handler(func):
+        async def _handler(data, websocket, path):
+            socket_id = server.socket_id(websocket)
+            response = func(data, socket_id)
+            blob = json.dumps({
+                "_id": data.get("_id", None),
+                "response": response
+            })
+            await websocket.send(blob)
+
+        server.add_handler(func.__name__, _handler)
+        return func
+    return _server_handler
+
+
+if __name__ == "__main__":
+    server = create_server()
+
+    asyncio.get_event_loop().run_until_complete(server.serve())
+    asyncio.get_event_loop().run_forever()
+    print("All done")
