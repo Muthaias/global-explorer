64892b7ac2bcb5553c293f984a20e5268aefb3e9
Mattias Nyberg <muthaias.uu@gmail.com>
2020-12-22 23:34:33 +0100

    wip: add data input action type to vue

--- assets/components/ge-action.js
+++ assets/components/ge-action.js
@@ -23,18 +23,39 @@ Vue.component("ge-action", {
     },
     template: (
 `
-<div class="menu-item"
-    :id="id"
-    v-on:click="triggerAction"
-    :class="{disabled: !enabled}"
->{{ title }}</div>
+<span>
+    <span class="menu-group"
+        v-if="type === 'input'"
+        :class="{disabled: !enabled}"
+    >
+        <input type="text" v-model="value" :placeholder="title">
+        <div class="menu-item"
+            v-on:click="triggerAction"
+            :class="{disabled: !enabled}"
+        >{{ title }}</div>
+    </span>
+    <div class="menu-item"
+        v-else
+        v-on:click="triggerAction"
+        :class="{disabled: !enabled}"
+    >{{ title }}</div>
+</span>
 `
     ),
+    data() {
+        return {
+            value: "",
+        }
+    },
     computed: {
         triggerAction() {
             return () => {
                 console.log("action:", this.id)
-                this.onAction({id: this.id})
+                if (this.type === "input") {
+                    this.onAction({id: this.id, value: this.value})
+                } else {
+                    this.onAction({id: this.id})
+                }
             }
         }
     }
